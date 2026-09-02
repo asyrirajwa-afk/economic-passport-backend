af14fbe5f11812478ac6f80b0bb255dbaf26aa14
asyrirajwa-afk <asyrirajwa@gmail.com>
2026-09-03 06:06:30 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5500",
-        "http://127.0.0.1:5500"
+        "http://127.0.0.1:5500",
         "https://glorious-laughter-production-0cac.up.railway.app"
     ],
     allow_credentials=True,
